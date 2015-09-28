b0c62e6fee4eac251b640813dd78547e0510efe7
Roger Logan <roger@anoralabs.com>
2015-09-28 18:31:07 -0500

    working on bin.py which will be used with testflow.py for later use

--- modules/common.py
+++ modules/common.py
@@ -2,6 +2,20 @@ import sys
 import os
 
 
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+
 def progress_tracker(lineno):
     if lineno%1000 == 0:
         if lineno==1000:

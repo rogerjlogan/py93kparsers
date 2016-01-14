7c4fa763932a3e1f41a31dcb341c34552669403b
Roger <roger@anoralabs.com>
2016-01-14 13:53:21 +0100

    tweaked example shell scripts

--- example_runs/ti_binning_keplerR20.sh
+++ example_runs/ti_binning_keplerR20.sh
@@ -1,11 +1,11 @@
 #!/bin/csh
 unsetenv PYTHONPATH
 unsetenv LD_LIBRARY_PATH
-/db/kepler_pe/93k/anaconda_ete/bin/python /db/kepler_pe/93k/py93kParsers/working/ti_binning.py\
-    -tp /db/kepler_pe/93k/py93kParsers/working/example_progs/KEPLERI8220/testprog/KEPLERI8220.tpg\
+/db/kepler_pe/93k/anaconda_ete/bin/python /db/kepler_pe/93k/working/py93kParsers/ti_binning.py\
+    -tp /db/kepler_pe/93k/working/py93kParsers/example_progs/KEPLERI8220/testprog/KEPLERI8220.tpg\
     -name kepler_r20\
     -bin BinningKepler_pg2.csv\
-    -out /db/kepler_pe/93k/py93kParsers/working/example_runs/kepler_r20\
+    -out /db/kepler_pe/93k/working/py93kParsers/example_runs/kepler_r20\
     -tt2c FT_RPC_HT\
     -pic png
     -c

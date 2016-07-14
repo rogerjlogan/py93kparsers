735dede4f237c76e9cf5193858b632820c752d72
Roger <roger@anoralabs.com>
2016-07-14 00:53:46 -0500

    updated matchloop_fixer_omap5.sh with latest settings after MatchLoopFixer.py complete

--- py93kParsers/example_runs/matchloop_fixer_omap5.sh
+++ py93kParsers/example_runs/matchloop_fixer_omap5.sh
@@ -1,36 +1,4 @@
 #!/bin/bash
-#Description: Dynamically modifies the RPTV counts to find the first passing
-#condition of an MATCHLOOP flattened pattern. Considers the fact that these
-#patterns have multiple ports and the modulo behavior needs to be preserved for LMAP.
-#
-#optional arguments:
-#  -h, --help            show this help message and exit
-#  -name NAME, --name NAME
-#                        Optional name used for output files/logs.
-#  -d, --debug           print a lot of debug stuff to dlog
-#  -out OUTPUT_DIR, --output_dir OUTPUT_DIR
-#                        Directory to place log file(s).
-#  -max MAXLOGS, --maxlogs MAXLOGS
-#                        (0=OFF:log data to stdout).Set to 1 to keep only one
-#                        log (subsequent runs will overwrite).
-#  -comm_port COMMENT_PORT, --comment_port COMMENT_PORT
-#                        Port that has the search comments (just give one port,
-#                        even if others have the string.
-#  -cmt_offst COMMENT_OFFSET, --comment_offset COMMENT_OFFSET
-#                        Vector comment address offset from matchloop.
-#                        (0=comm_srch_str is on matchloop repeat vector;
-#                        -1=comm_srch_str is one vector BEFORE matchloop START;
-#                        1=comm_srch_str is one vector AFTER matchloop END)
-#  -comm_srch_str COMMENT_SEARCH_STRING, --comment_search_string COMMENT_SEARCH_STRING
-#                        String to search for in comments
-#  -keep_temp_burst KEEP_TEMP_BURST, --keep_temp_burst KEEP_TEMP_BURST
-#                        Do not delete the temporary burst at the end of the
-#                        script exeuction (not the default)
-#  -skip_func, --skip_func
-#                        Do not run an initial functional test... not the
-#                        default !
-#  -bin, --bin           Dse a binary search algorithm instead of linear... not
-#                        the default !
 
 unset PYTHONPATH
 unset LD_LIBRARY_PATH
@@ -51,9 +19,9 @@ else
 fi
 
 $python $script\
-    -d\
     -name omap5\
     -out /var/opt/hp93000/soc/MatchLoopFixer\
     -comm_port pOTHER\
     -cmt_offst 1\
+    -bin
 
